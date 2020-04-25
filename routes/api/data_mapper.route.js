9aa11121f5955d116885c4e53106afc7c7672ce2
oladayosprinble <oladayo.agbolade@sprinble.co>
2020-04-25 16:05:03 +0100

    Feat: load provider data

--- routes/api/data_mapper.route.js
+++ routes/api/data_mapper.route.js
@@ -5,31 +5,38 @@ let mongoose = require("mongoose"),
 // Model
 let dataMapperModel = require("../../models/data_mapper");
 
+// Show all data
 router.route("/data").get((req, res, next) => {
+  dataMapperModel.find((error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+// Filter data
+router.route("/filter").get((req, res, next) => {
   res.send("It workds");
 });
 
 // Create data specification
 router.route("/create").post((req, res, next) => {
-  // console.log(req.body.providerId);
-  // console.log(req.body.fields.name);
-  // console.log(req.body.fields.age);
-  // return;
   const dataSpecification = new dataMapperModel({
-    providerId: 12345,
+    providerId: req.body.providerId,
     fields: [
       {
-        name: "Oladayo",
-        age: 23,
-        timestamp: 1587614729
+        name: req.body.fields[0].name,
+        age: req.body.fields[0].age,
+        timestamp: req.body.fields[0].timestamp
       }
     ]
   });
 
   dataSpecification
     .save()
-    // .then(res => res.status(200).json({ success: "true" }))
-    .then(res => console.log(res))
+    .then(response => res.status(200).json({data: response, success: "true" }))
     .catch(err => console.log(err));
 });
 
--- server.js
+++ server.js
@@ -1,37 +1,44 @@
 let express = require("express");
+let bodyParser = require("body-parser");
 const app = express();
+
 let mongoose = require("mongoose");
 let cors = require("cors");
-let bodyParser = require("body-parser");
+
 let dbConfig = require("./config/db");
 
-// Express Route
-const data = require('./routes/api/data_mapper.route');
-
-// Connecting mongoDB Database
-mongoose.Promise = global.Promise;
-mongoose.connect(dbConfig.db, {
-  useNewUrlParser: true
-}).then(() => {
-  console.log('Database sucessfully connected!')
-},
-  error => {
-    console.log('Could not connect to database : ' + error)
-  }
-)
-
-app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
 );
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 app.use(cors());
+
+// Express Route
+const data = require("./routes/api/data_mapper.route");
+
 app.use("/api", data);
 
 app.get("/", (req, res) => {
   res.send("Test endpoint");
 });
+
+// Connecting mongoDB Database
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(dbConfig.db, {
+    useNewUrlParser: true
+  })
+  .then(
+    () => {
+      console.log("Database sucessfully connected!");
+    },
+    error => {
+      console.log("Could not connect to database : " + error);
+    }
+  );
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`app listening on port ${port}`));
@@ -41,7 +48,7 @@ app.use((req, res, next) => {
   next(createError(404));
 });
 
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   console.error(err.message);
   if (!err.statusCode) err.statusCode = 500;
   res.status(err.statusCode).send(err.message);
